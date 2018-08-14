276d81faf34699b336a2b958be225e66a041ad78
YoungGi Hong <topyk21@gamil.com>
2018-08-14 18:01:19 +0900

    Add private-route

---
+++ src/lib/react-router/private-route.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import { Redirect, Route, RouteProps } from 'react-router-dom'
+
+interface IRouteProps extends RouteProps {
+  isAuth: boolean
+}
+
+const PrivateRoute: React.SFC<IRouteProps> = ({ component, isAuth, ...rest }) => {
+  const renderFn = (Component: any) => (props: RouteProps) => {
+    if (isAuth) {
+      return <Component {...props} />
+    }
+    const redirectProps = {
+      to: {
+        pathname: '/signin',
+        state: { from: props.location },
+      },
+    }
+    return <Redirect {...redirectProps} />
+  }
+
+  return <Route {...rest} render={renderFn(component)} />
+}
+
+export default PrivateRoute
