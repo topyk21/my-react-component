2f58501caf0fdd5b0cb23bba3f74f53a72663618
YoungGi Hong <topyk21@gamil.com>
2019-01-26 21:01:18 +0900

    checkbox: Add index file

---
+++ src/components/checkbox/index.ts
@@ -0,0 +1,9 @@
+import Checkbox from 'src/components/checkbox/Checkbox'
+import Togglebox from 'src/components/checkbox/Togglebox'
+
+export { Checkbox, Togglebox }
+
+export default {
+  Checkbox,
+  Togglebox,
+}
