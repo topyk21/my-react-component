2bbd0951a4b555c8a87346383d547d0cb93b473a
YoungGi Hong <topyk21@gamil.com>
2019-02-16 15:39:25 +0900

    checkbox:  Modify scss import position

--- src/components/checkbox/index.ts
+++ src/components/checkbox/index.ts
@@ -1,6 +1,9 @@
 import Checkbox from 'src/components/checkbox/Checkbox'
 import Togglebox from 'src/components/checkbox/Togglebox'
 
+import 'src/components/checkbox/Checkbox.scss'
+import 'src/components/checkbox/Togglebox.scss'
+
 export { Checkbox, Togglebox }
 
 export default {
