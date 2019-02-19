1cb674c2c58058a54d6c536b36b203781b60d75f
YoungGi Hong <topyk21@gamil.com>
2019-02-19 10:21:09 +0900

    file-uploaer: Move import position of css

--- src/components/file-uploader/index.ts
+++ src/components/file-uploader/index.ts
@@ -1,3 +1,5 @@
 import FileUploader from 'src/components/file-uploader/view/FileUploaderContainer'
 
+import 'src/components/file-uploader/view/FileUploader.scss'
+
 export default FileUploader
--- src/pages/test/Test2.tsx
+++ src/pages/test/Test2.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react'
 
+import FileUploader from 'src/components/file-uploader'
+
 class Test1 extends React.Component<{}, {}> {
   render() {
-    return <div>Hello! Test page2!</div>
+    return (
+      <div>
+        Hello! Test page2!
+        <FileUploader multiple api="" allowedExtension={['jpg', 'png']} />
+      </div>
+    )
   }
 }
 
