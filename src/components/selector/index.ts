6dbe1ddc5ba6fd379b2c54f266208489047adada
YoungGi Hong <topyk21@gamil.com>
2019-03-17 15:08:09 +0900

    selector: Renaming api selector

--- src/components/selector/index.ts
+++ src/components/selector/index.ts
@@ -1,7 +1,7 @@
 import Selector, {
   IContProps as SelectorProps,
 } from 'src/components/selector/view/SelectorContainer'
-import ApiSelector from 'src/components/selector/view/ApiSelector'
+import ApiSelector from 'src/components/selector/view/SelectorContainerWithAjax'
 import { SelectorItem } from 'src/components/selector/types'
 
 import 'src/components/selector/Selector.scss'
