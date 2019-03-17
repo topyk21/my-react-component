0ed499c639f76612901dcf2b1bd5c311c0ddb230
YoungGi Hong <topyk21@gamil.com>
2019-03-17 15:47:52 +0900

    Add board test page

--- src/pages/test/Test3.tsx
+++ src/pages/test/Test3.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react'
+import Board from 'src/components/board'
 
-class Test1 extends React.Component<{}, {}> {
-  render() {
-    return <div>Hello! Test page3!</div>
-  }
-}
+const Test3: React.SFC<{}> = () => (
+  <Board
+    api="https://api.jikan.moe/v3/search/anime"
+    cols={[
+      { key: 'mal_id', value: 'ID', width: 60 },
+      { key: 'title', value: 'Title', width: 210 },
+      { key: 'type', value: 'Type', width: 60 },
+      { key: 'score', value: 'Score', width: 60 },
+    ]}
+    dataKey="results"
+    dataUniqueKey="mal_id"
+    lastPageKey="last_page"
+    searchOptions={[{ key: 'q', value: 'Anime title' }]}
+  />
+)
 
-export default Test1
+export default Test3
